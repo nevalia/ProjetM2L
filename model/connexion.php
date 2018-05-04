f55e58858678e81278e0d19b45b31ebd9cd14e8b
Maha Amar <amarmaha2707@gmail.com>
2018-05-04 14:56:37 +0200

    ..

--- model/connexion.php
+++ model/connexion.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=mamar;charset=utf8", "root", "WX10wTwuvRbj3XXD",
+    $bdd = new PDO("mysql:host=localhost;dbname=mamar;charset=utf8", "mamar", "WX10wTwuvRbj3XXD",
         array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
         ));
